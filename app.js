4be6f2f873fe8146456544722e8e0b7aee6a546d
Dina <dinagiacobbi@gmail.com>
2024-01-25 08:17:20 -0900

    Change port information

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const port = 5000
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
+const cors = require('cors')
+
 require('dotenv').config()
 
 //when we hit any request, need to run the bodyParser - use() middleware:
 app.use(bodyParser.json())
+app.use(cors())
 
 //import routes
 const mainRoute = require('./routes/main')
@@ -16,41 +19,23 @@ app.use('/main', mainRoute)
 
 const URL = process.env.MONGODB_URI
 
-const { MongoClient, ServerApiVersion } = require('mongodb')
-
 app.get('/', (req, res) => {
   res.send('The home page.')
 })
 
 //Connect to database with Mongoose:
-mongoose.connect(URL)
-.then(() => console.log("Pinged your deployment. You successfully connected to MongoDB!"))
-.catch((err) => console.error(err))
-
-
-//MONGODB - successful connection:
-// const client = new MongoClient(URL, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
+mongoose
+  .connect(URL)
+  .then(() =>
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
+  )
+  .catch((err) => console.error(err))
 
 //LISTEN TO SERVER:
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT || port, () => {
+  console.log(
+    `Example app listening on port ${process.env.PORT || port}`
+  )
 })
--- routes/main.js
+++ routes/main.js
@@ -1,26 +1,66 @@
 const express = require('express')
 const router = express.Router()
-
 const Post = require('../models/Post')
 
-router.get('/', (req, res) => {
-  res.send('The main page.')
+router.get('/', async (req, res) => {
+  try {
+    // below is the promise that will be sent back from the database
+    const posts = await Post.find()
+    res.json(posts)
+  } catch (error) {
+    res.json({ message: error })
+  }
 })
 
-router.post('/', (req, res) => {
-  // console.log(req.body)
+router.post('/', async (req, res) => {
   const post = new Post({
     title: req.body.title,
-    description: req.body.description
+    description: req.body.description,
   })
-  //returns a promise:
-  post.save()
-  .then(data => {
-    res.json(data)
-  })
-  .catch(err =>{
-    res.json({message: err})
-  })
+
+  try {
+    // below is the promise
+    const savedData = await post.save()
+    res.json(savedData)
+  } catch (error) {
+    res.json({ message: error })
+  }
 })
 
+//get a specific data by using the model (schema):
+router.get('/:postId', async (req, res) => {
+  // console.log(req.params.postId)
+  // below is the promise
+  try {
+    const post = await Post.findById(req.params.postId)
+    res.json(post)
+  } catch (error) {
+    res.json({ message: error })
+  }
+})
+
+//delete data
+router.delete('/:postId', async (req, res) => {
+  try {
+    const deletedPost = await Post.deleteOne({
+      _id: req.params.postId,
+    })
+    res.json(deletedPost)
+  } catch (error) {
+    res.json({ message: error })
+  }
+})
+
+//update information ($set is just a specical operator to update a value)
+router.patch('/:postId', async (req, res) => {
+  try {
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } }
+    )
+    res.json(updatedPost)
+  } catch (error) {
+    res.json({ message: error })
+  }
+})
 module.exports = router
