791393c62cfa4f5893ed40f52458b2b6e1b9acec
Dina <dinagiacobbi@gmail.com>
2024-02-03 09:34:28 -0900

    Edits to UserSchema

--- models/User.js
+++ models/User.js
@@ -1,25 +1,7 @@
 const mongoose = require('mongoose')
 
 const UserSchema = mongoose.Schema({
-  firstName: {
-    type: String,
-    required: [true, "First name required"]
-  },
-
-  lastName: {
-    type: String,
-    required: [true, "Last name required"]
-  },
-
-  email: {
-    type: String,
-    required: [true, "Enter your email"]
-  },
-  //this takes more with authentication
-  password: {
-    type: String,
-    required: [true, "Password needed. Shhhh... keep it secret"]
-  }
+  
 })
 
 module.exports = mongoose.model('Users', UserSchema)
