544d3c5009986ccdefd80ca7fa3c9d4f608aceef
Dina <dinagiacobbi@gmail.com>
2024-02-06 10:31:42 -0900

    Updating editting User Schema

--- models/User.js
+++ models/User.js
@@ -1,7 +1,34 @@
 const mongoose = require('mongoose')
 
 const UserSchema = mongoose.Schema({
-  
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: false,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  healthHabits: [
+    {
+      date: {
+        type: Date,
+        default: Date.now,
+        required: true,
+      },
+      habit: {
+        type: String,
+        isCompleted: false,
+        required: true,
+      },
+    },
+  ],
 })
 
 module.exports = mongoose.model('Users', UserSchema)
