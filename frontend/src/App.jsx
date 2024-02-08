ca0f2dab9a1a69f90d0a4be5c1beb1bc22cbe575
Dina <dinagiacobbi@gmail.com>
2024-02-08 08:37:51 -0900

    Adding front end habit component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Habits from './components/habits'
+
+function App() {
+  return <Habits />
+}
+
+export default App
---
+++ frontend/src/components/habits.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+
+
+const Habits = () => {
+  return (
+   <> 
+   <Button variant="contained">Contained</Button>
+    {/* look at checkboxes */}
+    <Checkbox />
+   </>
+  )
+}
+
+export default Habits
